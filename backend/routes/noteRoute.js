319f65ed26aefe2ca6cf6d141319968f40e20c89
ahmedhesein1 <ahmedhesein1790@gmail.com>
2024-11-05 14:37:05 +0200

    Adding the Protect Middelware to Allow only Signed up users to use the application

--- backend/routes/noteRoute.js
+++ backend/routes/noteRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as userControl from "../controllers/userControl.js";
 import * as noteControl from "../controllers/noteControl.js";
 const router = express.Router();
+router.use(userControl.protect);
 router.route("/").get(noteControl.getAllNotes).post(noteControl.addNote);
 router
   .route("/:id")
