d4339154ad1a05929579df31ad46c0768190df01
ahmedhesein1 <ahmedhesein1790@gmail.com>
2024-11-05 16:05:19 +0200

    Adding the logout Route

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,7 +1,8 @@
-import express from 'express';
-import *as userControl from '../controllers/userControl.js';
+import express from "express";
+import * as userControl from "../controllers/userControl.js";
 const router = express.Router();
-router.post('/signup', userControl.signup);
-router.post('/login', userControl.login);
+router.post("/signup", userControl.signup);
+router.post("/login", userControl.login);
+router.post("/logout", userControl.logout);
 
-export default router
+export default router;
