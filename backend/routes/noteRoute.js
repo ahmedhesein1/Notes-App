9f1d930ae5f6ae473d41a4930db1023b7ff09842
ahmedhesein1 <ahmedhesein1790@gmail.com>
2024-11-05 13:13:09 +0200

    Fixing A Bug

--- backend/routes/noteRoute.js
+++ backend/routes/noteRoute.js
@@ -2,11 +2,9 @@ import express from "express";
 import * as userControl from "../controllers/userControl.js";
 import * as noteControl from "../controllers/noteControl.js";
 const router = express.Router();
+router.route("/").get(noteControl.getAllNotes).post(noteControl.addNote);
 router
-  .route("/")
-  .get(userControl.protect, noteControl.getAllNotes)
-  .post(userControl.protect, noteControl.addNote);
-router("/:id")
+  .route("/:id")
   .get(noteControl.getNote)
   .patch(noteControl.updateNote)
   .delete(noteControl.deleteNote);
